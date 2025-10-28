9fab898688cc006f296f3e1a7ac82372fef79edc
ozturkeniss <enisozturk75@gmail.com>
2025-10-28 04:06:55 +0300

    Import necessary packages for gRPC server implementation, including protobuf timestamp and GORM for database interactions.

--- internal/warrior/grpc_server.go
+++ internal/warrior/grpc_server.go
@@ -7,6 +7,8 @@ import (
 
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/types/known/timestamppb"
+	"gorm.io/gorm"
 )
 
 // WarriorServiceServer implements the WarriorService gRPC interface
