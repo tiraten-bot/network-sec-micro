ffc8f05bd7ae3c66f7115a4373a23b7d612fe9fe
ozturkeniss <enisozturk75@gmail.com>
2025-11-07 02:32:40 +0300

    zxcvzxcvzxcb

--- internal/arenaspell/database.go
+++ internal/arenaspell/database.go
@@ -1,50 +1,48 @@
 package arenaspell
 
 import (
-    "context"
-    "fmt"
-    "log"
-    "time"
+	"context"
+	"fmt"
+	"log"
+	"time"
 
-    "network-sec-micro/pkg/secrets"
+	"network-sec-micro/pkg/secrets"
 
-    "go.mongodb.org/mongo-driver/mongo"
-    "go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 var (
-    Client    *mongo.Client
-    DB        *mongo.Database
-    SpellColl *mongo.Collection
+	Client    *mongo.Client
+	DB        *mongo.Database
+	SpellColl *mongo.Collection
 )
 
 // InitDatabase initializes MongoDB for arenaspell
 func InitDatabase() error {
-    uri := getEnv("MONGODB_URI", "mongodb://localhost:27017")
-    dbName := getEnv("MONGODB_DB", "arenaspell_db")
+	uri := getEnv("MONGODB_URI", "mongodb://localhost:27017")
+	dbName := getEnv("MONGODB_DB", "arenaspell_db")
 
-    ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-    defer cancel()
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
 
-    var err error
-    Client, err = mongo.Connect(ctx, options.Client().ApplyURI(uri))
-    if err != nil {
-        return fmt.Errorf("failed to connect to MongoDB: %w", err)
-    }
+	var err error
+	Client, err = mongo.Connect(ctx, options.Client().ApplyURI(uri))
+	if err != nil {
+		return fmt.Errorf("failed to connect to MongoDB: %w", err)
+	}
 
-    if err := Client.Ping(ctx, nil); err != nil {
-        return fmt.Errorf("failed to ping MongoDB: %w", err)
-    }
+	if err := Client.Ping(ctx, nil); err != nil {
+		return fmt.Errorf("failed to ping MongoDB: %w", err)
+	}
 
-    DB = Client.Database(dbName)
-    SpellColl = DB.Collection((Spell{}).CollectionName())
+	DB = Client.Database(dbName)
+	SpellColl = DB.Collection((Spell{}).CollectionName())
 
-    log.Println("ArenaSpell MongoDB connection established")
-    return nil
+	log.Println("ArenaSpell MongoDB connection established")
+	return nil
 }
 
 func getEnv(key, def string) string {
-    return secrets.GetOrDefault(key, def)
+	return secrets.GetOrDefault(key, def)
 }
-
-
