1f2ca123c7b07ddb9a97b4453ab7bbc34f274536
ozturkeniss <enisozturk75@gmail.com>
2025-11-08 02:29:39 +0300

    Add Terraform version and provider configurations in versions.tf

---
+++ infra/global/versions.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_version = "~> 1.7"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.46"
+    }
+
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.29"
+    }
+
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.11"
+    }
+  }
+}
