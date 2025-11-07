044e57fe85fa50870bf192a851c612a3400ede83
ozturkeniss <enisozturk75@gmail.com>
2025-11-08 02:30:21 +0300

    Add output definitions for shared VPC ID and Route53 hosted zone ID in outputs.tf

---
+++ infra/global/outputs.tf
@@ -0,0 +1,9 @@
+# output "shared_vpc_id" {
+#   description = "ID of the shared services VPC"
+#   value       = aws_vpc.shared.id
+# }
+
+# output "dns_zone_id" {
+#   description = "Route53 hosted zone ID for root domain"
+#   value       = aws_route53_zone.root.zone_id
+# }
